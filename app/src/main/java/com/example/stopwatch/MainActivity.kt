d3933989fe5a6d968be6decced73b85ea63cb36c
yeonuk44 <yeonuk44@gmail.com>
2023-07-24 15:42:39 +0900

    # Add layout elements

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn_start : Button
+    private lateinit var btn_reset : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
